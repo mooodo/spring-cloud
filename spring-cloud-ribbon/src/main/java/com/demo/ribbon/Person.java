1a68d6f2f89055cc5e72b758a70f5f41e3b7f332
Fangang <fangang2004@126.com>
2022-08-20 01:04:08 +0800

    the first commit

---
+++ spring-cloud-ribbon/src/main/java/com/demo/ribbon/Person.java
@@ -0,0 +1,16 @@
+/*
+ * Created by 2020-05-03 20:07:26
+ */
+package com.demo.ribbon;
+
+import lombok.Data;
+
+/**
+ * @author fangang
+ */
+@Data
+public class Person {
+    private int id;
+    private String name;
+    private String gender;
+}
